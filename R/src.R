be13ae9421d5df4039e940cf5643558015cd4e4e
stnava <stnava@gmail.com>
2018-04-21 14:11:23 -0400

    ENH: update to usable versions

--- R/src.R
+++ R/src.R
@@ -1,28 +1,205 @@
+#' rcBasis
+#'
+#' Learn a rcissus image basis
+#'
+#' @param x input filenames or image list
+#' @param patchRadius patch radius, integer value
+#' @return basis is output
+#' @author Avants BB
+#' @seealso \code{\link[ANTsRCore]{ripmmarcPop}} \url{https://antsx.github.io/ANTsRCore/reference/ripmmarcPop.html}
+#' @examples
+#'
+#' pop = getANTsRData( "population" ) # list of example images
+#' imgBases = rcBasis( pop )
+#'
+#' @export rcBasis
+#' @importFrom utils write.csv
+#' @import ANTsR
+#' @importFrom ANTsRCore antsImageRead
+#' @importFrom ANTsRCore antsImageWrite
+#' @importFrom ANTsRCore imageFileNames2ImageList
+#' @importFrom ANTsRCore getMask
+#' @importFrom ANTsRCore ripmmarc
+#' @importFrom ANTsRCore ripmmarcPop
+#' @importFrom ANTsRCore randomMask
+#' @import methods
+#' @import h2o
+rcBasis <- function( x, patchRadius = 3  ) {
+  maskType = 'dense'
+  if ( ! ( maskType %in% c("sparse","dense") ) ) stop("pass dense or sparse as maskType" )
+  if ( length( x ) < 1 ) stop( "pass in a non-zero length input" )
+
+  if ( class( x[[1]] )[1] == 'character' )
+    isFilename = TRUE else isFilename = FALSE
+
+  # build a training dataset by sampling each image and binding together
+  # the output of the matrices
+  nsam = 1000
+  popmasks = list()
+  for ( i in 1:length( x ) ) {
+    if ( isFilename ) img = ANTsRCore::antsImageRead( x[ i ] ) else img = x[[ i ]]
+    if ( maskType == 'dense' )
+      popmasks[[ i ]] = ANTsRCore::getMask( img, cleanup = 0  )
+    if ( maskType == 'sparse' ) {
+      temp = ANTsRCore::getMask( img, cleanup = 0 )
+      popmasks[[ i ]] = ANTsRCore::randomMask( temp, nsam, perLabel = TRUE ) * temp
+      }
+    }
+  if ( isFilename ) {
+    rp = ANTsRCore::ripmmarcPop( ANTsRCore::imageFileNames2ImageList( x ),
+      popmasks, patchRadius=patchRadius, meanCenter = FALSE, patchSamples=nsam )
+    } else {
+    rp = ANTsRCore::ripmmarcPop( x,
+      popmasks, patchRadius=patchRadius, meanCenter = FALSE, patchSamples=nsam )
+    }
+  return( rp )
+}
+
+
+
+
+#' rcTrainingMatrix
+#'
+#' Build a rcissus training matrix
+#'
+#' @param y input filenames or image list defining ground truth
+#' @param x input filenames or image list defining one training modality
+#' @param masks input filenames or image list defining training modality masks
+#' @param rcb input image basis from \code{\link{rcBasis}}
+#' @param patchRadius patch radius, integer value
+#' @param nsamples number of samples per image
+#' @param seeds random seeds for reproducibility across training modalities
+#' @return training matrix and vector (ground truth) is output in list
+#' @author Avants BB
+#' @seealso \code{\link[ANTsRCore]{ripmmarcPop}} \url{https://antsx.github.io/ANTsRCore/reference/ripmmarcPop.html}
+#'
+#' @export rcTrainingMatrix
+rcTrainingMatrix <- function( y, x, masks, rcb, patchRadius = 3, nsamples = 1000, seeds ) {
+  if ( length( x ) < 1 ) stop( "pass in a non-zero length input" )
+  if ( length( x ) != length( y ) ) stop( "length of y should equal length of x" )
+
+  if ( class( x[[1]] )[1] == 'character' )
+    isFilename = TRUE else isFilename = FALSE
+
+  if ( missing( seeds ) )
+    seeds = sample( 1:10000 )[1:length(x)]
+
+  if ( length( x ) != length( seeds ) ) stop( "length of seeds should equal length of x" )
+
+  if (
+    ( class( y[[1]] )[1] == 'character' & !isFilename ) |
+    ( class( y[[1]] )[1] != 'character' &  isFilename )  )
+    stop(" both x and y should be either filenames or image lists")
+
+  if (
+    ( class( masks[[1]] )[1] == 'character' & !isFilename ) |
+    ( class( masks[[1]] )[1] != 'character' &  isFilename )  )
+    stop(" both x and masks should be either filenames or image lists")
+
+  # build a training dataset by sampling each image and binding together
+  # the output of the matrices
+  yvec = matrix( )
+  xmat = matrix( )
+  for ( i in 1:length( x ) ) {
+    if ( isFilename ) yimg = ANTsRCore::antsImageRead( y[ i ] ) else yimg = y[[ i ]]
+    if ( isFilename ) img = ANTsRCore::antsImageRead( x[ i ] ) else img = x[[ i ]]
+    if ( isFilename ) msk = ANTsRCore::antsImageRead( masks[ i ] ) else msk = masks[[ i ]]
+    ripped = ripmmarc( img, msk, patchRadius = patchRadius, meanCenter = FALSE,
+        patchSamples = nsamples,
+        evecBasis = rcb$basisMat, patchVarEx = nrow(rcb$basisMat),
+        canonicalFrame = rcb$canonicalFrame, regressProjections = TRUE,
+        rotationInvariant = FALSE )
+    if ( i == 1 ) {
+      xmat = ripped$evecCoeffs
+      yvec = matrix( yimg[  msk == 1 ], ncol = 1 )
+      spatialMat = imageDomainToSpatialMatrix( msk, msk )
+      } else {
+      xmat = rbind( xmat, ripped$evecCoeffs )
+      yvec = rbind( yvec, matrix( yimg[  msk == 1 ], ncol = 1 ) )
+      spatialMat = rbind( spatialMat, imageDomainToSpatialMatrix( msk, msk ) )
+      }
+    }
+
+  return( list( x = xmat, y = yvec, position = spatialMat ) )
+
+}
+
+
+
+
 #' rcTrain
 #'
 #' Train a rcissus model
 #'
-#' @param x input filenames
+#' @param y outcome vector
+#' @param trainingDf input training data
 #' @return model is output
 #' @author Avants BB
 #' @seealso \code{\link[ANTsRCore]{ripmmarcPop}} \url{https://antsx.github.io/ANTsRCore/reference/ripmmarcPop.html}
-#' @examples
-#' pop = getANTsRData( "population" ) # list of example images
-#' popmasks = list( )
-#' for ( i in 1:length( pop ) )
-#'   popmasks[[ i ]] = getMask( pop[[ i ]] )
-#' rp = ripmmarcPop( pop, popmasks, patchRadius=3,
-#'   meanCenter = TRUE, patchSamples=1000 )
-#' nv = 15
-#' rippedTest <- ripmmarc( pop[[3]], popmasks[[3]], patchRadius = 3,
-#'   evecBasis = rp$basisMat[1:nv,], patchVarEx = nv, meanCenter = TRUE,
-#'   canonicalFrame = rp$canonicalFrame, regressProjections = TRUE )
-#' mm = makeImage( popmasks[[3]], rippedTest$evecCoeffs[,1] )
 #'
 #' @export rcTrain
-#' @import ANTsR
-#' @import methods
-#' @import h2o
-rcTrain <- function( x ) {
-  return( NA )
+rcTrain <- function( y, trainingDf ) {
+  localH2O <- h2o::h2o.init( nthreads = -1 , max_mem_size = "100G" ) # all cores
+  h2o::h2o.init()
+  tempath = tempfile( pattern = "h2ofile", tmpdir = tempdir(), fileext = ".csv")
+  trainingDf$y = y
+  write.csv( trainingDf, tempath, row.names = FALSE )
+  train.h2o <- h2o::h2o.importFile( tempath )
+  mdl = h2o::h2o.deeplearning( y = "y",  training_frame = train.h2o, epochs=200 )
+  return( mdl )
+}
+
+#' rcTestingMatrix
+#'
+#' Build a rcissus testing matrix
+#'
+#' @param x input filenames or image list defining one testing modality
+#' @param masks input filenames or image list defining testing modality masks
+#' @param rcb input image basis from \code{\link{rcBasis}}
+#' @param patchRadius patch radius, integer value
+#' @param nsamples number of samples per image
+#' @param seeds random seeds for reproducibility across testing modalities
+#' @return testing matrix is output in list
+#' @author Avants BB
+#'
+#' @export rcTestingMatrix
+rcTestingMatrix <- function( x, masks, rcb, patchRadius = 3, nsamples = 1000, seeds ) {
+
+  if ( length( x ) < 1 ) stop( "pass in a non-zero length input" )
+
+  if ( class( x[[1]] )[1] == 'character' )
+    isFilename = TRUE else isFilename = FALSE
+
+  if ( missing( seeds ) )
+    seeds = sample( 1:10000 )[1:length(x)]
+
+  if ( length( x ) != length( seeds ) ) stop( "length of seeds should equal length of x" )
+
+  if (
+    ( class( masks[[1]] )[1] == 'character' & !isFilename ) |
+    ( class( masks[[1]] )[1] != 'character' &  isFilename )  )
+    stop(" both x and masks should be either filenames or image lists")
+
+  # build a training dataset by sampling each image and binding together
+  # the output of the matrices
+  xmat = matrix( )
+  for ( i in 1:length( x ) ) {
+    if ( isFilename ) img = ANTsRCore::antsImageRead( x[ i ] ) else img = x[[ i ]]
+    if ( isFilename ) msk = ANTsRCore::antsImageRead( masks[ i ] ) else msk = masks[[ i ]]
+    ripped = ripmmarc( img, msk, patchRadius = patchRadius, meanCenter = TRUE,
+        patchSamples = nsamples,
+        evecBasis = rcb$basisMat, patchVarEx = nrow(rcb$basisMat),
+        canonicalFrame = rcb$canonicalFrame, regressProjections = TRUE,
+        rotationInvariant = FALSE )
+    if ( i == 1 ) {
+      xmat = ripped$evecCoeffs
+      spatialMat = imageDomainToSpatialMatrix( msk, msk )
+      } else {
+      xmat = rbind( xmat, ripped$evecCoeffs )
+      spatialMat = rbind( spatialMat, imageDomainToSpatialMatrix( msk, msk ) )
+      }
+    }
+
+  return( list( x = xmat, position = spatialMat ) )
+
 }
--- README.md
+++ README.md
@@ -6,9 +6,61 @@ fast approach to mapping between image representations or modalities
 devtools::install_github("stnava/rcissus")
 ```
 
-an example
+an example predicting raw intensity from gradient and laplacian images
 
 ```
+
 library( rcissus )
-# FIXME
+popfns = getANTsRData('show')[1:5]
+testfn = getANTsRData('show')[6]
+# below, we use image lists but one can alternatively replaces lists
+# with vectors of filenames - just do so consistently
+popGT = list( )  # ground truth
+popGR = list( )  # gradient
+popLA = list( )  # laplacian
+masks = list( )  # sample masks
+nsam = 5000 # samples
+myPR = 3
+for ( i in 1:length( popfns ) ) {
+  popGT[[ i ]] = antsImageRead( getANTsRData( popfns[ i ] ) )
+  popGR[[ i ]] = iMath( popGT[[ i ]], "Grad", 1 )
+  popLA[[ i ]] = iMath( popGT[[ i ]], "Laplacian", 1 )
+  masks[[ i ]] = randomMask( thresholdImage( popGT[[ i ]], 1 , 255  ) , nsam )
+  }
+trnBas = rcBasis( lappend( popGR, popLA  ), patchRadius = myPR )
+trnBas$basisMat = trnBas$basisMat[ 1:8,  ] # select 15 basis vectors
+myseeds = c( 1:length( popGT ) )
+trnMat1 = rcTrainingMatrix( popGT, popGR, masks, trnBas, seeds = myseeds, patchRadius = myPR  )
+trnMat2 = rcTrainingMatrix( popGT, popLA, masks, trnBas, seeds = myseeds, patchRadius = myPR  )
+print( table( trnMat2$y==trnMat1$y ) ) # should be equivalent
+
+
+# build similar data for prediction - but use a dense mask
+popGTtest = list( )  # ground truth
+popGRtest = list( )  # gradient
+popLAtest = list( )  # laplacian
+maskstest = list( )  # sample masks
+for ( i in 1:length( testfn ) ) {
+  popGTtest[[ i ]] = antsImageRead( getANTsRData( testfn[ i ] ) )
+  popGRtest[[ i ]] = iMath( popGTtest[[ i ]], "Grad", 1 )
+  popLAtest[[ i ]] = iMath( popGTtest[[ i ]], "Laplacian", 1 )
+  maskstest[[ i ]] = getMask( popGTtest[[ i ]] ) # NOTE: dense prediction!
+  }
+
+testMat1 = rcTestingMatrix( popGRtest, maskstest, trnBas, seeds = 1, patchRadius = myPR )
+testMat2 = rcTestingMatrix( popLAtest, maskstest, trnBas, seeds = 1, patchRadius = myPR )
+
+# now implement the training and testing
+traindf = data.frame( trnMat1$x, trnMat2$x, trnMat1$position )
+testdf = data.frame( testMat1$x, testMat2$x, testMat1$position )
+library( randomForest )
+library( e1071 )
+# model interactions with position
+trn = svm(  trnMat1$y ~  ( . ) * X1 * X2 , data = traindf )
+prd = predict( trn, newdata = testdf )
+mm = makeImage( maskstest[[1]], prd  )
+plot( mm )
+# trn = rcTrain( trnMat1$y, traindf )
+# prd = rcPredict( trn, trnMat )
+
 ```
